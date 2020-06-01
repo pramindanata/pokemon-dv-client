3db5d5efda77ec3005455a0cb054d4d27ca5660f
pramindanata <pramindanata.eksa@gmail.com>
2020-06-01 19:20:23 +0800

    add nprogress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,15 @@
 import PropTypes from 'prop-types'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import '~/assets/index.scss'
+
+NProgress.configure({
+  showSpinner: false,
+})
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
